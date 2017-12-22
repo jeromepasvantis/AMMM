44e69939fe332d746e77873cf90b59b690c32c9a
kymry burwell <kymry.burwell@gmail.com>
2017-12-22 16:40:35 +0100

    Commented decoder code

--- BRKGA/decoder.py
+++ BRKGA/decoder.py
@@ -1,9 +1,23 @@
 import numpy as np
 import dataset1 as data
 
+'''
+Instructions for use - Simply update the 'import data' to the correct file and run
+Dataset format is as follows:
+nNurses = 100
+hours =  24
+demand = [ 18,19,17,18,20,21,23,24,24,28,32,32,33,30,29,28,27,26,25,20,18,18,18,19 ]
+minHours = 4
+maxHours = 10
+maxConsec = 5
+maxPresence = 13
+'''
+
+# Generate chromosome length
 def getChromosomeLength(data):
     return data.nNurses
 
+# Iterates through each individual in the population
 def decode(population, data):
     for ind in population:
         solution, fitness=decoder_assignment(data,ind['chr'])
@@ -11,25 +25,27 @@ def decode(population, data):
         ind['fitness']=fitness
     return(population)
 
+# Decoder - Adds hours to nurses and generates solution
 def decoder_assignment(data, chromosome):
-    nurses = list([])
+    nurses = list([]) # Create emply list for working nurses
     demand = list(data.demand)
+    # Iterate through each gene in Chromosome until demand is met
     for index, i in enumerate(chromosome):
-        
         if demandMet(demand):
             solution,fitness = generateSolution(nurses)
             return solution, fitness
         startHour = int(i)
         currentHour = startHour
-        nurse = Nurse(index)
-        if startHour % 2 == 0:
+        nurse = Nurse(index) x
+        if startHour % 2 == 0: # Sets working direction
             backwards = False
         else:
             backwards = True
-        nurses.append(nurse)
+        nurses.append(nurse) # Adds nurse to current solution
         maxPresence = data.maxPresence
         maxConsec = data.maxConsec
         consec = 0
+        #Adds hours to current nurse.
         while maxPresence > 0 and sum(nurse.hours) < data.maxHours:
             maxPresence -=  1
             if consec < maxConsec:
@@ -58,6 +74,7 @@ def demandMet(demand):
             met = False
     return met
 
+# Generate Solution
 def generateSolution(nurses):
     fitness = len(nurses)
     solution = list([])
@@ -65,6 +82,7 @@ def generateSolution(nurses):
         solution.append((n.name,n.hours))
     return solution, fitness
 
+# Nurse data structure
 class Nurse:
     def __init__(self,number):
         self.name = number
